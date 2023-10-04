b0623ae3b341842a0a9c8d4648c857a3ca8b4611
Tik Long Lee <leetiklong@gmail.com>
2023-10-04 20:47:30 +0800

    [install.sh / init] change install.sh sturcture

--- install.sh
+++ install.sh
@@ -1,25 +1,14 @@
 #!/usr/bin/env zsh
-if [[ -z $DOTFILES ]]; then
-    DOTFILES=$HOME/.dotfiles
-fi
+DOTFILES=$HOME/.dotfiles
+STOW_FOLDERS=(zsh vim neovim)
+cd $DOTFILES
 
-if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS=(zsh vim neovim)
-fi
+for file in ./init/*; do
+    echo "zsh $file"
+    zsh $file
+done
 
-if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-fi
-
-if [[ ! -f "$HOME/.vim/autoload/plug.vim" ]]; then
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-pushd $DOTFILES
-for folder in $STOW_FOLDERS
-do
+for folder in $STOW_FOLDERS; do
     echo "stow $folder"
     stow $folder
 done
-popd

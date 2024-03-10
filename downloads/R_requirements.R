7e2b335a2499bab095fd80b41e7ec567877895f7
Tik Long Lee <leetiklong@gmail.com>
2024-03-11 02:42:27 +0800

    [downloads/R] list of R libraries downloaded

---
+++ downloads/R_requirements.R
@@ -0,0 +1,25 @@
+# Jupyter + VSCode
+install.packages("IRkernel")
+IRkernel::installspec()
+install.packages("languageserver")
+
+# R Markdown
+install.packages("markdown")
+install.packages("rmarkdown")
+install.packages("formatR")
+
+# STAT 4001
+install.packages("ISLR") # datasets
+install.packages("rafalib") # helper functions
+install.packages("glmnet") # ridge + lasso
+install.packages("gam") # generalized additive models
+install.packages("tree") # decision / regression tree
+install.packages("randomForest") # random forest
+install.packages("gbm") # boosting tree
+
+# Useful Packages
+install.packages("tidyverse") # data science collections
+install.packages("ggtern") # ternary diagrams
+
+# Misc
+install.packages("zeallot") # unpack multiple assignments
